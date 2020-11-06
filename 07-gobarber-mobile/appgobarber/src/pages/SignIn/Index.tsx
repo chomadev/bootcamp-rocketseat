f26150eb992a0e1d2518ff9b447d9320e39e8042
Marcos Choma <marcos.choma@gmail.com>
2020-11-05 23:30:45 -0300

    App creation, routes, sign in and up structure

---
+++ 07-gobarber-mobile/appgobarber/src/pages/SignIn/Index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Text>Sign in</Text>
+    </Container>
+  );
+}
+
+export default SignIn;
---
+++ 07-gobarber-mobile/appgobarber/src/pages/SignUp/Index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>Sign in</Text>
+    </Container>
+  );
+}
+
+export default SignUp;
